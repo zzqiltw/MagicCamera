1c1542b1c7babf42c953aa23293c9a3091aea57b
郑志勤 <hzzhengzhiqin@corp.netease.com>
2017-03-02 19:02:01 +0800

    project

---
+++ MagicCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagicCamera
+//
+//  Created by 郑志勤 on 2017/3/2.
+//  Copyright © 2017年 zzqiltw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MagicCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MagicCamera
+//
+//  Created by 郑志勤 on 2017/3/2.
+//  Copyright © 2017年 zzqiltw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
